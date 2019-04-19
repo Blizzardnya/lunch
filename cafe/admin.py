4049b74340db9781c2ca03cd24c1482270d57632
UserWEB <joni_95@tut.by>
2019-04-19 15:54:03 +0300

    Add filters and search to admin

--- cafe/admin.py
+++ cafe/admin.py
@@ -8,6 +8,8 @@ from .models import Category, Product
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'available', 'created_at', 'updated_at')
     list_editable = ('available', )
+    list_filter = ('available', 'created_at', 'updated_at')
+    search_fields = ['name']
     prepopulated_fields = {'slug': ('name', )}
 
 
@@ -15,4 +17,6 @@ class CategoryAdmin(admin.ModelAdmin):
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'category', 'price', 'available', 'created_at', 'updated_at')
     list_editable = ('price', 'available')
+    list_filter = ('available', 'created_at', 'updated_at')
+    search_fields = ['name', 'category__name']
     prepopulated_fields = {'slug': ('name', )}
