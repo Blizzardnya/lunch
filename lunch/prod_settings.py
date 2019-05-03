9fc2bf1a38851887ece3863d673d90e1b06bf496
UserWEB <joni_95@tut.by>
2019-05-03 10:54:17 +0300

    Fix Heroku deploy configuration (DEBUG value)

--- lunch/prod_settings.py
+++ lunch/prod_settings.py
@@ -4,8 +4,6 @@ import dj_database_url
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
-DEBUG = bool(os.environ.get('DJANGO_DEBUG', False))
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
